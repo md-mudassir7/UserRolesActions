ffe6b79a58b0a7b86f342b2eee26289824bb88a6
mdmudassir7 <mohammedmudassir200@gmail.com>
2022-03-17 12:45:55 +0530

    Update models.py

--- models.py
+++ models.py
@@ -4,6 +4,13 @@ from enum import Enum
 
 class resourceClass(BaseModel):
     name:str
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Name of the resource",
+            }
+        }
+
 
 
 class actions(str,Enum):
@@ -12,14 +19,27 @@ class actions(str,Enum):
     ex = "execute"
 
 
-
 class rolesClass(BaseModel):
     name:str
     class objs(BaseModel):
         action : actions
         resource : resourceClass
     permissions : List[objs]
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Name of the role",
+                "permissions": ["List of the permissions"]
+            }
+        }
 
 class userClass(BaseModel):
     name: str
     roles: List[str]
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Name of the user",
+                "roles": ["List of the roles"]
+            }
+        }
